bb9800648fd5977116d9d150a5e4a7748407f10e
kangwoo <kangwoo111@naver.com>
2025-08-12 06:37:20 +0900

    json출력(스택문제)

--- Backend/kwtest/workspace/run_ceevizor/main.c
+++ Backend/kwtest/workspace/run_ceevizor/main.c
@@ -1,11 +1,21 @@
-// 03_stdout_concat.c
 #include <stdio.h>
 
-int main(void) {
-  printf("A");         // 개행 없음
-  printf("B\n");       // 여기서 A와 B가 함께 한 토큰으로 붙어야 함
-  puts("C line");      // 무조건 개행
-  putchar('D');        // 개행 없음
-  putchar('\n');       // 여기서 D가 붙음
+// 함수 선언 (함수 원형)
+int add(int a, int b);
+
+int main() {
+  int num1 = 11, num2 = 5;
+  int sum;
+
+  // 함수 호출
+  sum = add(num1, num2);
+
+  printf("두 수의 합: %d\n", sum);
+
   return 0;
 }
+
+// 함수 정의
+int add(int a, int b) {
+  return a + b;
+}
